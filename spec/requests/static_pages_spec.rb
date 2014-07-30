417fa62b505d8d946c91954b3dbc2946ac764cef
hbaughman <hbaughman741@gmail.com>
2014-07-30 19:04:09 -0400

    Configure spec_helper to use Spork; guard for automated tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "RoR Tutorial" }
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title "RoR Tutorial | Home"
+      expect(page).to have_title "#{base_title} | Home"
     end
   end
 
@@ -21,7 +23,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title "RoR Tutorial | Help"
+      expect(page).to have_title "#{base_title} | Help"
     end
   end
 
@@ -32,7 +34,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title "RoR Tutorial | About Us"
+      expect(page).to have_title "#{base_title} | About Us"
     end
   end
 
@@ -42,8 +44,8 @@ describe "StaticPages" do
       expect(page).to have_content('Contact Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title "RoR Tutorial | Contact Us"
+      visit '/static_pages/contact'
+      expect(page).to have_title "#{base_title} | Contact Us"
     end
   end
 
