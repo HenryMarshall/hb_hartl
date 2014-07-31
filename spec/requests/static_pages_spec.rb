da929500ae03efa9e0db59e97097ecb0082edf27
hbaughman <hbaughman741@gmail.com>
2014-07-30 21:12:15 -0400

    Update specs to reflect default title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,6 +13,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       expect(page).to have_title "#{base_title}"
+      expect(page).not_to have_title "| Home"
     end
   end
 
