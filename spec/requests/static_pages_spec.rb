04baf0a4f9e139096c2cbbd718ac13aabb83afc5
hbaughman <hbaughman741@gmail.com>
2014-07-31 22:50:07 -0400

    Update Static pages spec to use shared_examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,29 +7,43 @@ describe "StaticPages" do
   # let(:base_title) { "RoR Tutorial" }
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home Page" do
     before { visit root_path }
-    it { should have_content('hartl tutorial') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home')}
+    let(:heading)     { 'Sample App'}
+    let(:page_title)  { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading)     { 'About Us' }
+    let(:page_title)  { 'About Us' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Us" do
     before { visit contact_path }
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_title(full_title('Contact Us')) }
+    let(:heading)     { 'Contact Us' }
+    let(:page_title)  { 'Contact Us' }
+    it_should_behave_like "all static pages"
+
+    # deprecated for shared examples (5.3.9)
+    # it { should have_selector('h1', text: 'Contact Us') }
+    # it { should have_title(full_title('Contact Us')) }
 
     # deprecated long form (5.3.4)
     # it "should have the right title" do
