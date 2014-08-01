4df9f86f0cd97a1e1fe237332ee0e93883bc677a
hbaughman <hbaughman741@gmail.com>
2014-07-31 21:48:20 -0400

    Update spec to use rails routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,12 @@ describe "StaticPages" do
 
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title "#{base_title}"
       expect(page).not_to have_title "| Home"
     end
@@ -19,33 +19,33 @@ describe "StaticPages" do
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title "#{base_title} | Help"
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title "#{base_title} | About Us"
     end
   end
 
   describe "Contact Us" do
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Us')
     end
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title "#{base_title} | Contact Us"
     end
   end
