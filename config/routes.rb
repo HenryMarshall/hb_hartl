5f4389ea9f79fe5adca0538ab1ce641f601aec15
hbaughman <hbaughman741@gmail.com>
2014-08-09 13:12:00 -0400

    Add users#show and gravatars

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ HbHartl::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  resources :users
+
   match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
