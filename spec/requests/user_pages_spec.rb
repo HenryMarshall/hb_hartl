0dc1c181120eef0caff8df94bf26ae4739b02095
hbaughman <hbaughman741@gmail.com>
2014-08-11 16:34:19 -0400

    Create specs and view for user#edit

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,4 +57,15 @@ describe "UserPages" do
     it { should have_title(user.name) }
   end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+  end
+
 end
