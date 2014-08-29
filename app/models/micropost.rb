d5b4ea81b7e119875ee80b2a5779b27a5f84b451
hbaughman <hbaughman741@gmail.com>
2014-08-29 10:42:47 -0400

    Update from_users_followed_by for better scale

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,11 +6,19 @@ class Micropost < ActiveRecord::Base
 
 
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    # The above is a shorthand equivalent for:
+    # this code is heavily revamped for efficiency in 11.3.3
+
+    followed_user_ids = "SELECT followed_id FROM relationships
+                        WHERE follower_id = :user_id"
+
+    # The following are equivalent, both were deprecated for subset selection
+    # followed_user_ids = user.followed_user_ids
     # followed_user_ids = User.first.followed_users.map(&:id)
 
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user)
+    # Deprecated for above to improve efficiency in situations where a user is
+    # subscribed to many other users (e.g., 5000)
   end
 
 end
